9828f4066fad6b9fcbef6706267bcc13a35e1530
henimezrani <henimezrani@gmail.com>
2019-11-07 23:02:14 +0100

    modified js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ***************** Global vars
-var plus = [1,2,3];
-var zero = [0,0,0];
-var minus = [-1,-2,-3];
+var plus = [1, 2, 3];
+var zero = [0, 0, 0];
+var minus = [-1, -2, -3];
 
 var iterations = {
     i: [plus, plus, zero, minus, minus, minus, zero, plus],
@@ -12,19 +12,47 @@ var iterations = {
 
 // ***************** Constructors
 
-function Game(){
+function Game() {
     var game = {};
 
     game.playBoard = [];
+    game.player1 = {
+        name: "",
+        number: 1,
+        color: 'red',
+        score: 0,
+        start: true
+    }
+    game.player2 = {
+        name: "",
+        number: 2,
+        color: 'yellow',
+        score: 0,
+        start: false
+    }
+    game.totalPlays = 42;
+
 
     game.initialize = initialize;
     game.addValue = addValue;
     game.check = check;
+    game.updateScore = updateScore;
+    game.currentPlayer = currentPlayer;
+    //game.play = play;
+
     return game;
 }
 
 var initialize = function() {
-    this.playBoard = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+    this.playBoard = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ]
+
     /*var columns = [];
     for (var i = 0 ; i < 6 ; i++){
         columns[i] = 0
@@ -32,31 +60,38 @@ var initialize = function() {
     for (var i = 0 ; i < 7 ; i++) {
         this.playBoard[i] = columns
     }*/
+    this.totalPlays = 42;
 }
 
 var addValue = function(number, column) {
-    for (var i = this.playBoard.length-1; i >= 0; i--) {
-        if (this.playBoard[i][column] === 0){
+    var boo = true;
+    for (var i = this.playBoard.length - 1; i >= 0; i--) {
+        if (this.playBoard[i][column] === 0) {
             this.playBoard[i][column] = number
-            break;  
+            boo = false; // full colun
+            break;
         }
     }
+    if (!boo) {
+        // handle full columns
+    }
+
 }
 
 
 
-var check = function(){
-    for (var i = this.playBoard.length-1; i >= 0; i--) {
-        for (var j = this.playBoard[i].length-1; j >= 0; j--) {
-            for(var k = 0 ; k < 8 ; k++) {
+var check = function() {
+    for (var i = this.playBoard.length - 1; i >= 0; i--) {
+        for (var j = this.playBoard[i].length - 1; j >= 0; j--) {
+            for (var k = 0; k < 8; k++) {
                 var boo = true
 
                 var iIndexes = iterations.i[k].map(x => x + i);
                 var jIndexes = iterations.j[k].map(x => x + j);
 
 
-                for (var l = 0 ; l < iIndexes.length ; l++){
-                    if ( (iIndexes[l] < 0) || (jIndexes[l] < 0) || (iIndexes[l] >= this.playBoard.length) || (jIndexes >=this.playBoard[i].length) ){
+                for (var l = 0; l < iIndexes.length; l++) {
+                    if ((iIndexes[l] < 0) || (jIndexes[l] < 0) || (iIndexes[l] >= this.playBoard.length) || (jIndexes >= this.playBoard[i].length)) {
                         boo = false;
                     }
                 }
@@ -73,8 +108,8 @@ var check = function(){
                 jIndexes.filter(x => x>=this.playBoard[i].length)
 
                 */
-                if ( (this.playBoard[i][j] !==0) && (this.playBoard[i][j] === this.playBoard[iIndexes[0]][jIndexes[0]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[1]][jIndexes[1]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[2]][jIndexes[2]]) ){
-                    
+                if ((this.playBoard[i][j] !== 0) && (this.playBoard[i][j] === this.playBoard[iIndexes[0]][jIndexes[0]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[1]][jIndexes[1]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[2]][jIndexes[2]])) {
+
                     console.log("there is a correct answer")
                 }
             }
@@ -83,12 +118,47 @@ var check = function(){
     }
 }
 
+var updateScore = function(n) {
+    if (n === 1) {
+        this.player1.score++;
+        alert(this.player1.name + " wins!");
+
+    } else {
+        this.player2.score++;
+        alert(this.player2.name + " wins!");
+    }
+    this.initialize();
+}
+
+var currentPlayer = function() {
+    this.player1.start = !this.player1.start
+    this.player2.start = !this.player2.start
+}
+
+/*
+var play = function() {
+    $column.on('click', function() {
+
+        
+        //switch player
+        //check
+        // if true update score
+        //else keep playing
+
+
+
+
+    })
+}*/
+
+
+
 
 newGame = Game();
 newGame.initialize();
 console.table(newGame.playBoard);
-newGame.addValue(1,2)
-newGame.addValue(1,1)
-newGame.addValue(1,2)
-newGame.addValue(1,2)
-
+newGame.addValue(1, 2)
+newGame.addValue(1, 1)
+newGame.addValue(1, 2)
+newGame.addValue(1, 2)
+newGame.addValue(1, 2)
