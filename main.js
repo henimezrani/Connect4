dd45eb44a85cd52c3cc5f77971d6200b03efbfb0
henimezrani <henimezrani@gmail.com>
2019-11-08 23:59:17 +0100

    added welcome

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 
 
 
-
+//$('#welcome').hide()
 //$('#game').hide()
-$('#winner').hide()
-    // ***************** Global vars
+//$('#winner').hide()
+// ***************** Global vars
 var plus = [1, 2, 3];
 var zero = [0, 0, 0];
 var minus = [-1, -2, -3];
@@ -17,25 +17,25 @@ var iterations = {
     j: [zero, minus, minus, minus, zero, plus, plus, plus]
 }
 
-function Player(name, number, color){
+function Player(name, number, color) {
     var player = {};
 
     player.name = name,
-    player.number = number,
-    player.color = color,
-    player.score = 0
+        player.number = number,
+        player.color = color,
+        player.score = 0
 
     return player;
 }
 
 // ***************** Constructors
 
-function Game(player1name,player1color,player2name,player2color) {
+function Game(player1name, player1color, player2name, player2color) {
     var game = {};
 
     game.playBoard = [];
-    game.player1 = Player(player1name, 1, validateColor(player1color,"yellow"));
-    game.player2 = Player(player2name, 2, validateColor(player2color,"red"));
+    game.player1 = Player(player1name, 1, validateColor(player1color, "yellow"));
+    game.player2 = Player(player2name, 2, validateColor(player2color, "red"));
     game.totalPlays = 42;
     game.currentPlayer; // true when player one turn
 
@@ -54,7 +54,12 @@ var initialize = function(nCols, nRows) {
     this.playBoard = [];
     $('#playBoard').html('');
     this.currentPlayer = this.player1;
-
+    for (var i = 0; i < nCols; i++) {
+        $('#prePlayBoard').append('<div class="column" id="pre' + i + '"></div>')
+        for (var j = -1; j < 0; j++) {
+            $('#pre' + i).append('<div class="row" id="pre' + i + 'r' + j + '"><div>')
+        }
+    }
     for (var i = 0; i < nRows; i++) {
         var tmpArr = []
         for (var j = 0; j < nCols; j++) {
@@ -69,24 +74,6 @@ var initialize = function(nCols, nRows) {
             $('#' + i).append('<div class="row" id="c' + i + 'r' + j + '"><div>')
         }
     }
-
-    /*this.playBoard = [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]
-    ]*/
-
-    /*var columns = [];
-    for (var i = 0 ; i < 6 ; i++){
-        columns[i] = 0
-    }
-    for (var i = 0 ; i < 7 ; i++) {
-        this.playBoard[i] = columns
-    }*/
-
 
     this.totalPlays = nRows * nCols;
 }
@@ -129,11 +116,11 @@ var check = function() {
                 }
 
                 if (boo && (this.playBoard[i][j] === this.playBoard[iIndexes[0]][jIndexes[0]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[1]][jIndexes[1]]) && (this.playBoard[i][j] === this.playBoard[iIndexes[2]][jIndexes[2]])) {
-                    if (this.playBoard[i][j] !== 0){
+                    if (this.playBoard[i][j] !== 0) {
                         this.updateScore();
                     }
                 }
-                
+
             }
         }
     }
@@ -144,7 +131,7 @@ var check = function() {
 var updateScore = function() {
     this.currentPlayer.score++;
     alert(this.currentPlayer.name + " wins!");
-    this.initialize(7,6);
+    this.initialize(7, 6);
 }
 
 var updateCurrentPlayer = function() {
@@ -153,60 +140,74 @@ var updateCurrentPlayer = function() {
     } else {
         this.currentPlayer = this.player1;
     }
-    
+
 }
 
 
-newGame = Game("Heni", "blue", "opponent", "hur");
-newGame.initialize(7, 6);
-console.table(newGame.playBoard);
 
-$('.column').on('click', function() {
-    newGame.addValue($(this).attr('id'));
-    
-})
 
-$('#changeSize').on('click', function() {
-    //Make window aprear
-    // choose new size
-})
-
-$('#updateSize').on('click', function() {
-    var newCol = $('#changeCols').val()
-    var newRows = $('#changeCols').val()
-    newGame.initialize(newCol,newRows)
-})
-
-function validateColor(input,defaultVal){
-    var colorValidation = ["Pink", "LightPink", "HotPink", "DeepPink", "PaleVioletRed", "MediumVioletRed", "LightSalmon", "Salmon", "DarkSalmon", "LightCoral", "IndianRed", "Crimson", "Firebrick", "DarkRed", "Red", 
-    "OrangeRed", "Tomato", "Coral", "DarkOrange", "Orange", "Yellow", "LightYellow", "LemonChiffon", "LightGoldenrodYellow ", "PapayaWhip", "Moccasin", "PeachPuff", "PaleGoldenrod", "Khaki", "DarkKhaki", "Gold", 
-    "Cornsilk", "BlanchedAlmond", "Bisque", "NavajoWhite", "Wheat", "Burlywood", "Tan", "RosyBrown", "SandyBrown", "Goldenrod", "DarkGoldenrod", "Peru", "Chocolate", "SaddleBrown", "Sienna", "Brown", "Maroon", 
-    "DarkOliveGreen", "Olive", "OliveDrab", "YellowGreen", "LimeGreen", "Lime", "LawnGreen", "Chartreuse", "GreenYellow", "SpringGreen", "MediumSpringGreen ", "LightGreen", "PaleGreen", "DarkSeaGreen", "MediumAquamarine", 
-    "MediumSeaGreen", "SeaGreen", "ForestGreen", "Green", "DarkGreen", "Aqua", "Cyan", "LightCyan", "PaleTurquoise", "Aquamarine", "Turquoise", "MediumTurquoise", "DarkTurquoise", "LightSeaGreen", "CadetBlue", 
-    "DarkCyan", "Teal", "LightSteelBlue", "PowderBlue", "LightBlue", "SkyBlue", "LightSkyBlue", "DeepSkyBlue", "DodgerBlue", "CornflowerBlue", "SteelBlue", "RoyalBlue", "Blue", "MediumBlue", "DarkBlue", "Navy", 
-    "MidnightBlue", "Lavender", "Thistle", "Plum", "Violet", "Orchid", "Fuchsia", "Magenta", "MediumOrchid", "MediumPurple", "BlueViolet", "DarkViolet", "DarkOrchid", "DarkMagenta", "Purple", "Indigo", "DarkSlateBlue", 
-    "SlateBlue", "MediumSlateBlue ", "White", "Snow", "Honeydew", "MintCream", "Azure", "AliceBlue", "GhostWhite", "WhiteSmoke", "Seashell", "Beige", "OldLace", "FloralWhite", "Ivory", "AntiqueWhite", "Linen", 
-    "LavenderBlush", "MistyRose", "Gainsboro", "LightGray", "Silver", "DarkGray", "Gray", "DimGray", "LightSlateGray", "SlateGray", "DarkSlateGray", "Black"].map( x => x.toLowerCase());
+function validateColor(input, defaultVal) {
+    var colorValidation = ["Pink", "LightPink", "HotPink", "DeepPink", "PaleVioletRed", "MediumVioletRed", "LightSalmon", "Salmon", "DarkSalmon", "LightCoral", "IndianRed", "Crimson", "Firebrick", "DarkRed", "Red",
+        "OrangeRed", "Tomato", "Coral", "DarkOrange", "Orange", "Yellow", "LightYellow", "LemonChiffon", "LightGoldenrodYellow ", "PapayaWhip", "Moccasin", "PeachPuff", "PaleGoldenrod", "Khaki", "DarkKhaki", "Gold",
+        "Cornsilk", "BlanchedAlmond", "Bisque", "NavajoWhite", "Wheat", "Burlywood", "Tan", "RosyBrown", "SandyBrown", "Goldenrod", "DarkGoldenrod", "Peru", "Chocolate", "SaddleBrown", "Sienna", "Brown", "Maroon",
+        "DarkOliveGreen", "Olive", "OliveDrab", "YellowGreen", "LimeGreen", "Lime", "LawnGreen", "Chartreuse", "GreenYellow", "SpringGreen", "MediumSpringGreen ", "LightGreen", "PaleGreen", "DarkSeaGreen", "MediumAquamarine",
+        "MediumSeaGreen", "SeaGreen", "ForestGreen", "Green", "DarkGreen", "Aqua", "Cyan", "LightCyan", "PaleTurquoise", "Aquamarine", "Turquoise", "MediumTurquoise", "DarkTurquoise", "LightSeaGreen", "CadetBlue",
+        "DarkCyan", "Teal", "LightSteelBlue", "PowderBlue", "LightBlue", "SkyBlue", "LightSkyBlue", "DeepSkyBlue", "DodgerBlue", "CornflowerBlue", "SteelBlue", "RoyalBlue", "Blue", "MediumBlue", "DarkBlue", "Navy",
+        "MidnightBlue", "Lavender", "Thistle", "Plum", "Violet", "Orchid", "Fuchsia", "Magenta", "MediumOrchid", "MediumPurple", "BlueViolet", "DarkViolet", "DarkOrchid", "DarkMagenta", "Purple", "Indigo", "DarkSlateBlue",
+        "SlateBlue", "MediumSlateBlue ", "White", "Snow", "Honeydew", "MintCream", "Azure", "AliceBlue", "GhostWhite", "WhiteSmoke", "Seashell", "Beige", "OldLace", "FloralWhite", "Ivory", "AntiqueWhite", "Linen",
+        "LavenderBlush", "MistyRose", "Gainsboro", "LightGray", "Silver", "DarkGray", "Gray", "DimGray", "LightSlateGray", "SlateGray", "DarkSlateGray", "Black"
+    ].map(x => x.toLowerCase());
     var hexValidation = ["a", "b", "c", "d", "e", "f", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].join('')
 
     var hex = true;
     var htmlColor = true;
-    for (var i = 1 ; i < input.length ; i++) {
+    for (var i = 1; i < input.length; i++) {
         if (hexValidation.indexOf(input[i].toLowerCase()) < 0) {
             hex = false;
         }
-        if ( !( (input.length === 7 || input.length === 4) && (input[0] === "#") ) ) {
+        if (!((input.length === 7 || input.length === 4) && (input[0] === "#"))) {
             hex = false;
         }
     }
 
-    if (colorValidation.indexOf(input) < 0){
+    if (colorValidation.indexOf(input) < 0) {
         htmlColor = false;
     }
 
-    if (hex || htmlColor){
+    if (hex || htmlColor) {
         return input;
     }
     return defaultVal;
-
 }
+
+$('#startGame').on("click", function() {
+    var player1name = $('#player1name').val()
+    var player1color = $('#player1color').val()
+    var player2name = $('#player2name').val()
+    var player2color = $('#player2color').val()
+    newGame = Game(player1name, player1color, player2name, player2color);
+    newGame.initialize(7, 6);
+    console.table(newGame.playBoard);
+
+    $('.column').on('click', function() {
+        newGame.addValue($(this).attr('id'));
+    })
+
+
+
+    $('.column').mouseover(function() {
+        $('#pre' + $(this).attr('id') + 'r-1').css('background-color', newGame.currentPlayer.color)
+    })
+
+    $('.column').mouseout(function() {
+        $('#pre' + $(this).attr('id') + 'r-1').css('background-color', "white")
+    })
+
+
+    $('#updateSize').on('click', function() {
+        var newCol = $('#changeCols').val()
+        var newRows = $('#changeCols').val()
+        newGame.initialize(newCol, newRows)
+    })
+
+})
